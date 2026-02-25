12ca6536c77d752c5f69269c822f0d60382d1935
mawo-sgy <mawogueyesow07@gmail.com>
2026-02-25 08:02:30 +0100

    Add navbar and hero section

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 fetch('./data/exercises.json')
     .then(response => response.json())
     .then(data => {
-
         const exercises = data.exercises;
         const container = document.getElementById('exercises-container');
 
         exercises.forEach(exercise => {
             const card = document.createElement('div');
+            card.className = "bg-sky-600 max-w-lg rounded-lg text-white p-5";
             card.innerHTML = `
                 <h2>${exercise.title}</h2>
                 <p>${exercise.chapter}</p>
@@ -14,6 +14,5 @@ fetch('./data/exercises.json')
                 <p>${exercise.explanation}</p>
             `;
             container.appendChild(card)
-        })
-
+        });
     })
